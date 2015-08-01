d31446306b07f19e9b4906b8b69592b4c7ad73b8
SidTheSeer <sidtheseer@gmail.com>
2015-08-01 22:45:23 +1000

    Make code PEP8 friendly and center buttons

--- scenes.py
+++ scenes.py
@@ -1,30 +1,43 @@
 import gamelib.gamelib as gamelib
 
+
 class MainMenu(gamelib.MenuScene):
-	def __init__(self, director = None, buttons = None, background = None, music = None):
-		background = gamelib.BackgroundImage((0, 0, director.screen.get_rect().width, director.screen.get_rect().height), ['assets', 'images', 'test.jpg'], 'contain')
-		buttons = [gamelib.MainMenuButton(self, {'click': ['loadScene', 1]}, (0, 0, 500, 100), 'YARRR'), gamelib.MainMenuButton(self, None, (0, 300, 500, 100), 'DERP')]
-		music = None
+    def __init__(self, director=None, buttons=None, background=None, music=None):
+        buttonWidth = 300
+        buttonHeight = 100
+        w = director.screen.get_rect().width
+        h = director.screen.get_rect().height
+        wCenter = int((w - buttonWidth) / 2)
+        hCenter = int((h - buttonHeight) / 2)
 
-		#self.fadeInStuff = pygame.Surface((director.screen.get_rect().width, director.screen.get_rect().height))
-		#self.fadeInStuff.fill((0, 0, 0))
-		#self.alpha = 255
-		super().__init__(director, buttons, background, music)
+        background = gamelib.BackgroundImage((0, 0, w, h), ['assets', 'images', 'test.jpg'], 'cover')
+        playButton = gamelib.MainMenuButton(self, {'click': ['loadScene', 1]}, (wCenter, hCenter - 150, 300, 100), 'PLAY')
+        optionsButton = gamelib.MainMenuButton(self, None, (wCenter, hCenter, 300, 100), 'OPTIONS')
+        quitButton = gamelib.MainMenuButton(self, None, (wCenter, hCenter + 150, 300, 100), 'QUIT')
 
-		#self.text = gamelib.Text((820, 450, 500, 100), 'I like POTATO', DEFAULT_FONT, gamelib.BLACK)
+        buttons = [playButton, optionsButton, quitButton]
 
+        music = None
 
-	def handleCommand(self, command):
-		pass
+        # self.fadeInStuff = pygame.Surface((director.screen.get_rect().width, director.screen.get_rect().height))
+        # self.fadeInStuff.fill((0, 0, 0))
+        # self.alpha = 255
+        super().__init__(director, buttons, background, music)
+
+        # self.text = gamelib.Text((820, 450, 500, 100), 'I like POTATO', DEFAULT_FONT, gamelib.BLACK)
+
+    def handleCommand(self, command):
+        pass
+
 
 class MainMenu2(gamelib.MenuScene):
-	def __init__(self, director = None, buttons = None, background = None, music = None):
-		background = gamelib.BackgroundImage((0, 0, director.screen.get_rect().width, director.screen.get_rect().height), ['assets', 'images', 'test.jpg'], 'cover')
-		buttons = [gamelib.MainMenuButton(self, {'click': ['loadScene', 0]}, (0, 0, 500, 100), 'SCENE2'), gamelib.MainMenuButton(self, None, (0, 300, 500, 100), 'DERP')]
-		music = None
-		super().__init__(director, buttons, background, music)
+    def __init__(self, director=None, buttons=None, background=None, music=None):
+        background = gamelib.BackgroundImage((0, 0, director.screen.get_rect().width, director.screen.get_rect().height), ['assets', 'images', 'test.jpg'], 'cover')
+        buttons = [gamelib.MainMenuButton(self, {'click': ['loadScene', 0]}, (0, 0, 500, 100), 'SCENE2'), gamelib.MainMenuButton(self, None, (0, 300, 500, 100), 'DERP')]
+        music = None
+        super().__init__(director, buttons, background, music)
 
-		#self.text = gamelib.Text((820, 450, 500, 100), 'I like POTATO', DEFAULT_FONT, gamelib.BLACK)
+        # self.text = gamelib.Text((820, 450, 500, 100), 'I like POTATO', DEFAULT_FONT, gamelib.BLACK)
 
-	def handleCommand(self, command):
-		pass
+    def handleCommand(self, command):
+        pass
